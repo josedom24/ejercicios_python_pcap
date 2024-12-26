c8b4941549d703e45c98de6cf013b3c4f3f54ff6
José Domingo Muñoz <josedom24@gmail.com>
2024-12-26 13:37:39 +0100

    Seccion5

---
+++ seccion5/06 - Ordenación de cadenas/programa1.py
@@ -0,0 +1,14 @@
+# Ejemplo con sorted
+greek = ['omega', 'alpha', 'pi', 'gamma']
+greek_sorted = sorted(greek)
+
+print(greek)        # Lista original intacta
+print(greek_sorted) # Lista ordenada
+
+# Ejemplo con sort
+
+greek = ['omega', 'alpha', 'pi', 'gamma']
+
+print(greek)  # Lista original
+greek.sort()
+print(greek)  # Lista ordenada en su lugar

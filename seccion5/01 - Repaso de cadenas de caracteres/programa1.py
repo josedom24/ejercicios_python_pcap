e17521dbed3a14ab6d3c6e2dc2a988002464f8ed
José Domingo Muñoz <josedom24@gmail.com>
2024-12-26 11:57:28 +0100

    Seccion5

---
+++ seccion5/01 - Repaso de cadenas de caracteres/programa1.py
@@ -0,0 +1,10 @@
+cadena = 'Python3 es bonito'
+
+for indice in range(len(cadena)):
+    print(cadena[indice], end=' ')
+print()
+
+for caracter in cadena:
+    print(caracter, end=' ')
+print()
+
---
+++ seccion5/01 - Repaso de cadenas de caracteres/programa3.py
@@ -0,0 +1,13 @@
+alfabeto = "abcdefghijklmnopqrstuvwxyz"
+
+print("f" in alfabeto)
+print("F" in alfabeto)
+print("1" in alfabeto)
+print("ghi" in alfabeto)
+print("Xyz" in alfabeto)
+print()
+print("f" not in alfabeto)
+print("F" not in alfabeto)
+print("1" not in alfabeto)
+print("ghi" not in alfabeto)
+print("Xyz" not in alfabeto)

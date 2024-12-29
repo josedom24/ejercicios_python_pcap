1f96d55b5b538a8605dc8b85fc7b3e2b87ec62dc
José Domingo Muñoz <josedom24@gmail.com>
2024-12-29 18:56:05 +0100

    Sección8

--- seccion8/06 - LABORATORIO: Pila Contadora/programa.py
+++ seccion8/06 - LABORATORIO: Pila Contadora/programa.py
@@ -1,39 +1,31 @@
 class Stack:
     def __init__(self):
-        self.__stack_list = []
+        self.__stk = []
 
     def push(self, val):
-        self.__stack_list.append(val)
+        self.__stk.append(val)
 
     def pop(self):
-        val = self.__stack_list[-1]
-        del self.__stack_list[-1]
+        val = self.__stk[-1]
+        del self.__stk[-1]
         return val
 
 
-class AddingStack(Stack):
+class CountingStack(Stack):
     def __init__(self):
         Stack.__init__(self)
-        self.__sum = 0
+        self.__counter = 0
 
-    def get_sum(self):
-        return self.__sum
-
-    def push(self, val):
-        self.__sum += val
-        Stack.push(self, val)
+    def get_counter(self):
+        return self.__counter
 
     def pop(self):
-        val = Stack.pop(self)
-        self.__sum -= val
-        return val
+        self.__counter += 1
+        return Stack.pop(self)
 
 
-stack_object = AddingStack()
-
-for i in range(5):
-    stack_object.push(i)
-print(stack_object.get_sum())
-
-for i in range(5):
-    print(stack_object.pop())
+stk = CountingStack()
+for i in range(100):
+    stk.push(i)
+    stk.pop()
+print(stk.get_counter())

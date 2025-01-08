9ec18adf3f747a3e9968ebc8839b055eb1a5bfdb
José Domingo Muñoz <josedom24@gmail.com>
2025-01-08 18:24:18 +0100

    Seccion14

---
+++ seccion14/03 - El módulo datetime/programa2.py
@@ -0,0 +1,4 @@
+from datetime import date
+
+my_date = date(2019, 11, 4)
+print(my_date)
---
+++ seccion14/03 - El módulo datetime/programa3.py
@@ -0,0 +1,8 @@
+from datetime import date
+import time
+
+timestamp = time.time()
+print("Marca de tiempo:", timestamp)
+
+d = date.fromtimestamp(timestamp)
+print("Fecha:", d)
